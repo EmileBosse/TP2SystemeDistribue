b89a9d3c0019778b4cf7d010fe9e9f546ec33d67
Émile Bossé <emile.bosse@cidco.ca>
2018-03-19 11:27:32 -0400

    projet eclispe

---
+++ TP2/src/p4/mission4.java
@@ -0,0 +1,9 @@
+package p4;
+
+public class mission4 {
+	public static void main(String[] argv) throws Exception {
+		System.out.println("mission 4 démarre indépendaement ?");
+		System.in.read();
+		System.out.println("Mission 4 terminé!");
+	}
+}

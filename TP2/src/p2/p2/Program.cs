0c550422f6fdc553a05830cfef75f834c8a7bc40
jbiras <jbiras@users.noreply.github.com>
2018-03-26 11:08:37 -0400

    P2 et P3
    P2 en attente de faire fonctionner la magnifique API de google oui.
    
    P3 est supposé être fonctionnel

---
+++ TP2/src/p2/p2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+            var response = client.TranslateText("Hello World.", "ru");
+            Console.WriteLine(response.TranslatedText);
+            Console.ReadLine();
+
+        }
+    }
+}
